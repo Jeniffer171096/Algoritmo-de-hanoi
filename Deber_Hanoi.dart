7d65cf19d5a75a0c1829f5c45ad5b24ef61bdfc0
Jeniffer Guamanarca <71280031+Jeniffer171096@users.noreply.github.com>
2021-12-23 16:40:20 -0500

    Add files via upload

---
+++ Deber_Hanoi.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Se Formatea un movimiento a la salida.
+  String say(String from, String to) => "move $from -> $to"; 
+
+  //Hace un movimiento y activa de forma recursiva los siguientes movimientos, si los hay.
+  hanoi(int discs, String a, String b, String c) {
+    // Hace un movimiento solo si hay discos.
+    if (discs > 0) {
+      // Anuncia este movimiento, de A a C.
+      print(say(a, c));
+      //   Activa el siguiente paso: de A a B.
+      hanoi(discs - 1, a, c, b);
+      // Activa el último paso: de B a C.
+      hanoi(discs - 1, b, a, c);
+    }
+  }
+
+  
+  hanoi(13, '1', '2', '3');
+}
+   
